ab66befea1abcdd779e3e25f6adafbcddc6d7b42
Elias Stenhede Johansson <estenhedejohan@login04.delftblue.tudelft.nl>
2023-03-01 17:14:16 +0100

    handle the boundaries like dirichlet in apply_stencil3d

--- operations.cpp
+++ operations.cpp
@@ -29,23 +29,37 @@ void axpby(int n, double a, double const* x, double b, double* y)
 //! apply a 7-point stencil to a vector
 void apply_stencil3d(stencil3d const* S, double const* u, double* v)
 {
-  for (int ix=0; ix<S->nx; ix++) {
+  // Handle the boundaries, where we have dirichlet BC's. 
+	// We want to keep those values so just copy u to v, with the correct scaling.
+	for (int iz=0; iz<S->nz; iz++) {
 		for (int iy=0; iy<S->ny; iy++) {
-			for (int iz=0; iz<S->nz; iz++) {
+			v[S->index_c(0      , iy, iz)] =  S->value_c * u[S->index_c(0      , iy, iz)];
+			v[S->index_c(S->nx-1, iy, iz)] =  S->value_c * u[S->index_c(S->nx-1, iy, iz)];
+		}
+		for (int ix=0; ix<S->nx; ix++) {
+			v[S->index_c(ix, 0      , iz)] =  S->value_c * u[S->index_c(ix, 0      , iz)];
+			v[S->index_c(ix, S->ny-1, iz)] =  S->value_c * u[S->index_c(ix, S->ny-1, iz)];
+		}
+	}
+	for (int ix=1; ix<S->nx-1; ix++) {
+		for (int iy=1; iy<S->ny-1; iy++) {
+      int bot_idx = S->index_c(ix, iy, 0);
+			v[S->index_c(ix, iy, 0      )] =  S->value_c * u[S->index_c(ix, iy, 0      )];
+			v[S->index_c(ix, iy, S->nz-1)] =  S->value_c * u[S->index_c(ix, iy, S->nz-1)];
+		}
+	}
+	//For all interior points, calculate the laplacian
+	for (int iz=1; iz<S->nz-1; iz++) {
+		for (int iy=1; iy<S->ny-1; iy++) {
+			for (int ix=1; ix<S->nx-1; ix++) {
         int curr_idx = S->index_c(ix, iy, iz);
-				v[curr_idx]  = S->value_c * u[curr_idx];
-				if (ix > 0)
-					v[curr_idx] += S->value_w * u[S->index_w(ix, iy, iz)];
-				if (ix+1 < S->nx)
-					v[curr_idx] += S->value_e * u[S->index_e(ix, iy, iz)];
-				if (iy > 0)
-					v[curr_idx] += S->value_s * u[S->index_s(ix, iy, iz)];
-				if (iy+1 < S->ny)
-					v[curr_idx] += S->value_n * u[S->index_n(ix, iy, iz)];
-				if (iz > 0)
-					v[curr_idx] += S->value_t * u[S->index_t(ix, iy, iz)];
-				if (iz+1 < S->nz)
-					v[curr_idx] += S->value_b * u[S->index_b(ix, iy, iz)];
+				v[curr_idx] =  S->value_c * u[curr_idx];
+			  v[curr_idx] += S->value_w * u[S->index_w(ix, iy, iz)];
+			  v[curr_idx] += S->value_e * u[S->index_e(ix, iy, iz)];
+			  v[curr_idx] += S->value_s * u[S->index_s(ix, iy, iz)];
+			  v[curr_idx] += S->value_n * u[S->index_n(ix, iy, iz)];
+			  v[curr_idx] += S->value_t * u[S->index_t(ix, iy, iz)];
+			  v[curr_idx] += S->value_b * u[S->index_b(ix, iy, iz)];
 			}
 		}
 	}
