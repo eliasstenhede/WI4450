d2cfc492bffaeec72f97236049d5f5993a94432e
Elias Stenhede Johansson <estenhedejohan@login03.delftblue.tudelft.nl>
2023-05-02 08:59:27 +0200

    remove multiple passes over domain

--- operations.cpp
+++ operations.cpp
@@ -1,11 +1,11 @@
 #include "operations.hpp"
 #include "timer.hpp"
 #include <omp.h>
-
+#include <string>
 //Initialize x with constant value
 void init(long n, double* x, double const value)
 {
-	Timer t("init", 0/1e9, 8*n/1e9);
+	Timer t("init", 0/1e9, 2*8*n/1e9); //Load and store 
 #pragma omp parallel for schedule(static)
   for (int ix=0; ix<n; ix++) {
     x[ix] = value;
@@ -15,7 +15,7 @@ void init(long n, double* x, double const value)
 
 double dot(long n, double const* x, double const* y)
 {
-	Timer t("dot", n/1e9, 8*2*n/1e9); //Assuming fused multiply add, we read x and y
+	Timer t("dot", 2*n/1e9, 2*8*n/1e9); //Counting mult and add as separate operations, we read x and y
   double dot_result = 0.0;
 #pragma omp parallel for schedule(static) reduction(+:dot_result)
   for (int ix=0; ix<n; ix++) {
@@ -27,7 +27,7 @@ double dot(long n, double const* x, double const* y)
 //y = a*x+b*y
 void axpby(long n, double a, double const* x, double b, double* y)
 {
-	Timer t("axpby", 2*n/1e9, 8*3*n/1e9); //Assuming fused multiply add, we read x, y and write to y
+	Timer t("axpby", 3*n/1e9, 3*8*n/1e9); //Counting multiply and add as separate operations, we load x,y store y.
 #pragma omp parallel for schedule(static)
   for (int ix=0; ix<n; ix++) {
     y[ix] = a*x[ix]+b*y[ix];
@@ -38,84 +38,151 @@ void axpby(long n, double a, double const* x, double b, double* y)
 //Apply a 7-point stencil to a vector
 void apply_stencil3d(stencil3d const* S, double const* u, double* v)
 {
-	long nx = S->nx;
-	long ny = S->ny;
-	long nz = S->nz;
-	long n = nx*ny*nz;
-	Timer t("stencil", (7*n)/1e9, 8*2*n/1e9); //For all 7 loops we perform either multiplication or fused mult,add. We assume u and v are cached so we only read each of them once.
-#pragma omp parallel
-	{
-//#pragma omp for schedule(static) 
-  	/*for (int iz=1; iz<nz-1; iz++) {
-  	  for (int iy=1; iy<ny-1; iy++) {
-  	    for (int ix=1; ix<nx-1; ix++) {
-          v[S->index_c(ix, iy, iz)] = S->value_c * u[S->index_c(ix, iy, iz)];
-          v[S->index_c(ix, iy, iz)] += S->value_e * u[S->index_e(ix, iy, iz)];
-          v[S->index_c(ix, iy, iz)] += S->value_w * u[S->index_w(ix, iy, iz)];
-          v[S->index_c(ix, iy, iz)] += S->value_n * u[S->index_n(ix, iy, iz)];
-          v[S->index_c(ix, iy, iz)] += S->value_s * u[S->index_s(ix, iy, iz)];
-          v[S->index_c(ix, iy, iz)] += S->value_t * u[S->index_t(ix, iy, iz)];
-          v[S->index_c(ix, iy, iz)] += S->value_b * u[S->index_b(ix, iy, iz)];
-  		  }
-  		}
-  	}*/
+	long nn = S->nx*S->ny*S->nz;
+	Timer tt("separate", 13*nn/1e9, 3*8*nn/1e9); //Load u, load and store v
+	//A for loop over the three dimensions that applies the stencil S to vector u and stores it in v
 
-#pragma omp for schedule(static)   
-  	for (int iz=0; iz<nz; iz++) {
-  	  for (int iy=0; iy<ny; iy++) {
-  	    for (int ix=0; ix<nx; ix++) {
-          v[S->index_c(ix, iy, iz)] = S->value_c * u[S->index_c(ix, iy, iz)];
-  		  }
-  		}
-  	}
-#pragma omp for schedule(static)   
-  	for (int iz=0; iz<nz; iz++) {
-  	  for (int iy=0; iy<ny; iy++) {
-  	    for (int ix=0; ix<nx-1; ix++) {
-          v[S->index_c(ix, iy, iz)] += S->value_e * u[S->index_e(ix, iy, iz)];
-  		  }
-  		}
-  	}
-#pragma omp for schedule(static)   
-  	for (int iz=0; iz<nz; iz++) {
-  	  for (int iy=0; iy<ny; iy++) {
-  	    for (int ix=1; ix<nx; ix++) {
-          v[S->index_c(ix, iy, iz)] += S->value_w * u[S->index_w(ix, iy, iz)];
-  		  }
-  		}
-  	}
-#pragma omp for schedule(static)   
-  	for (int iz=0; iz<nz; iz++) {
-  	  for (int iy=0; iy<ny-1; iy++) {
-  	    for (int ix=0; ix<nx; ix++) {
-          v[S->index_c(ix, iy, iz)] += S->value_n * u[S->index_n(ix, iy, iz)];
-  		  }
-  		}
-  	}
-#pragma omp for schedule(static)   
-  	for (int iz=0; iz<nz; iz++) {
-  	  for (int iy=1; iy<ny; iy++) {
-  	    for (int ix=0; ix<nx; ix++) {
-          v[S->index_c(ix, iy, iz)] += S->value_s * u[S->index_s(ix, iy, iz)];
-  		  }
-  		}
-  	}
-#pragma omp for schedule(static)   
-  	for (int iz=0; iz<nz-1; iz++) {
-  	  for (int iy=0; iy<ny; iy++) {
-  	    for (int ix=0; ix<nx; ix++) {
-          v[S->index_c(ix, iy, iz)] += S->value_t * u[S->index_t(ix, iy, iz)];
-  		  }
-  		}
-  	}
-#pragma omp for schedule(static)   
-  	for (int iz=1; iz<nz; iz++) {
-  	  for (int iy=0; iy<ny; iy++) {
-  	    for (int ix=0; ix<nx; ix++) {
-          v[S->index_c(ix, iy, iz)] += S->value_b * u[S->index_b(ix, iy, iz)];
-  		  }
-  		}
-  	}
+	//    0,    0,    0
+	v[S->index_c(0,0,0)] = S->value_c*u[S->index_c(0,0,0)] + S->value_e*u[S->index_e(0,0,0)] + S->value_n*u[S->index_n(0,0,0)] + S->value_t*u[S->index_t(0,0,0)];
+	//  ...,    0,    0
+
+	#pragma omp parallel for schedule(static)
+	for(int i = 1; i < S->nx-1; i++){
+		v[S->index_c(i,0,0)] = S->value_c*u[S->index_c(i,0,0)] + S->value_e*u[S->index_e(i,0,0)] + S->value_w*u[S->index_w(i,0,0)] + S->value_n*u[S->index_n(i,0,0)] + S->value_t*u[S->index_t(i,0,0)];
 	}
+	// nx-1,    0,    0
+	v[S->index_c(S->nx-1,0,0)] = S->value_c*u[S->index_c(S->nx-1,0,0)] + S->value_w*u[S->index_w(S->nx-1,0,0)] + S->value_n*u[S->index_n(S->nx-1,0,0)] + S->value_t*u[S->index_t(S->nx-1,0,0)];
+
+	//    0,  ...,    0
+	#pragma omp parallel for schedule(static)
+	for(int j = 1; j < S->ny-1; j++){
+		v[S->index_c(0,j,0)] = S->value_c*u[S->index_c(0,j,0)] + S->value_e*u[S->index_e(0,j,0)] + S->value_n*u[S->index_n(0,j,0)] + S->value_s*u[S->index_s(0,j,0)] + S->value_t*u[S->index_t(0,j,0)];
+	}
+	//  ...,  ...,    0
+	#pragma omp parallel for schedule(static) collapse(2)
+	for(int j = 1; j < S->ny-1; j++){
+		for(int i = 1; i < S->nx-1; i++){
+			v[S->index_c(i,j,0)] = S->value_c*u[S->index_c(i,j,0)] + S->value_e*u[S->index_e(i,j,0)] + S->value_w*u[S->index_w(i,j,0)] + S->value_n*u[S->index_n(i,j,0)] + S->value_s*u[S->index_s(i,j,0)] + S->value_t*u[S->index_t(i,j,0)];
+		}
+	}
+	// nx-1,  ...,    0
+	#pragma omp parallel for schedule(static)
+	for(int j = 1; j < S->ny-1; j++){
+		v[S->index_c(S->nx-1,j,0)] = S->value_c*u[S->index_c(S->nx-1,j,0)] + S->value_w*u[S->index_w(S->nx-1,j,0)] + S->value_n*u[S->index_n(S->nx-1,j,0)]+ S->value_s*u[S->index_s(S->nx-1,j,0)] + S->value_t*u[S->index_t(S->nx-1,j,0)];
+	}
+
+	//    0, ny-1,    0
+	v[S->index_c(0,S->ny-1,0)] = S->value_c*u[S->index_c(0,S->ny-1,0)] + S->value_e*u[S->index_e(0,S->ny-1,0)] + S->value_s*u[S->index_s(0,S->ny-1,0)] + S->value_t*u[S->index_t(0,S->ny-1,0)];
+	//  ..., ny-1,    0
+	#pragma omp parallel for schedule(static)
+	for(int i = 1; i < S->nx-1; i++){
+		v[S->index_c(i,S->ny-1,0)] = S->value_c*u[S->index_c(i,S->ny-1,0)] + S->value_e*u[S->index_e(i,S->ny-1,0)] + S->value_w*u[S->index_w(i,S->ny-1,0)] + S->value_s*u[S->index_s(i,S->ny-1,0)] + S->value_t*u[S->index_t(i,S->ny-1,0)];
+	}
+	// nx-1, ny-1,    0
+	v[S->index_c(S->nx-1,S->ny-1,0)] = S->value_c*u[S->index_c(S->nx-1,S->ny-1,0)] + S->value_w*u[S->index_w(S->nx-1,S->ny-1,0)] + S->value_s*u[S->index_s(S->nx-1,S->ny-1,0)] + S->value_t*u[S->index_t(S->nx-1,S->ny-1,0)];
+
+
+
+	//    0,    0,  ...
+	#pragma omp parallel for schedule(static)
+	for(int k = 1; k < S->nz-1; k++){
+		v[S->index_c(0,0,k)] = S->value_c*u[S->index_c(0,0,k)] + S->value_e*u[S->index_e(0,0,k)] + S->value_n*u[S->index_n(0,0,k)] + S->value_t*u[S->index_t(0,0,k)] + S->value_b*u[S->index_b(0,0,k)];
+	}
+	//  ...,    0,  ...
+	#pragma omp parallel for schedule(static) collapse(2)
+	for(int k = 1; k < S->nz-1; k++){
+		for(int i = 1; i < S->nx-1; i++){
+			v[S->index_c(i,0,k)] = S->value_c*u[S->index_c(i,0,k)] + S->value_e*u[S->index_e(i,0,k)] + S->value_w*u[S->index_w(i,0,k)] + S->value_n*u[S->index_n(i,0,k)] + S->value_t*u[S->index_t(i,0,k)] + S->value_b*u[S->index_b(i,0,k)];
+		}
+	}
+	// nx-1,    0,  ...
+	#pragma omp parallel for schedule(static)
+	for(int k = 1; k < S->nz-1; k++){
+		v[S->index_c(S->nx-1,0,k)] = S->value_c*u[S->index_c(S->nx-1,0,k)] + S->value_w*u[S->index_w(S->nx-1,0,k)] + S->value_n*u[S->index_n(S->nx-1,0,k)] + S->value_t*u[S->index_t(S->nx-1,0,k)] + S->value_b*u[S->index_b(S->nx-1,0,k)];
+	}
+
+
+	//    0,  ...,  ...
+	#pragma omp parallel for schedule(static) collapse(2)
+	for(int k = 1; k < S->nz-1; k++){
+		for(int j = 1; j < S->ny-1; j++){
+			v[S->index_c(0,j,k)] = S->value_c*u[S->index_c(0,j,k)] + S->value_e*u[S->index_e(0,j,k)] + S->value_n*u[S->index_n(0,j,k)] + S->value_s*u[S->index_s(0,j,k)] + S->value_t*u[S->index_t(0,j,k)] + S->value_b*u[S->index_b(0,j,k)] ;
+		}
+	}
+	//  ...,  ...,  ...
+	#pragma omp parallel for schedule(static) collapse(3)
+	for(int k = 1; k < S->nz-1; k++){
+		for(int j = 1; j < S->ny-1; j++){
+			for(int i = 1; i < S->nx-1; i++){
+				v[S->index_c(i,j,k)] = S->value_c*u[S->index_c(i,j,k)] + S->value_e*u[S->index_e(i,j,k)] + S->value_w*u[S->index_w(i,j,k)] + S->value_n*u[S->index_n(i,j,k)] + S->value_s*u[S->index_s(i,j,k)] + S->value_t*u[S->index_t(i,j,k)] + S->value_b*u[S->index_b(i,j,k)];
+			}
+		}
+	}
+	// nx-1,  ...,  ...
+	#pragma omp parallel for schedule(static) collapse(2)
+	for(int k = 1; k < S->nz-1; k++){
+		for(int j = 1; j < S->ny-1; j++){
+			v[S->index_c(S->nx-1,j,k)] = S->value_c*u[S->index_c(S->nx-1,j,k)] + S->value_w*u[S->index_w(S->nx-1,j,k)] + S->value_n*u[S->index_n(S->nx-1,j,k)]+ S->value_s*u[S->index_s(S->nx-1,j,k)] + S->value_t*u[S->index_t(S->nx-1,j,k)] + S->value_b*u[S->index_b(S->nx-1,j,k)];
+		}
+	}
+
+	//    0, ny-1,  ...
+	#pragma omp parallel for schedule(static)
+	for(int k = 1; k < S->nz-1; k++){
+		v[S->index_c(0,S->ny-1,k)] = S->value_c*u[S->index_c(0,S->ny-1,k)] + S->value_e*u[S->index_e(0,S->ny-1,k)] + S->value_s*u[S->index_s(0,S->ny-1,k)] + S->value_t*u[S->index_t(0,S->ny-1,k)] + S->value_b*u[S->index_b(0,S->ny-1,k)];
+	}  
+	//  ..., ny-1,  ...
+	#pragma omp parallel for schedule(static) collapse(2)
+	for(int k = 1; k < S->nz-1; k++){
+		for(int i = 1; i < S->nx-1; i++){
+			v[S->index_c(i,S->ny-1,k)] = S->value_c*u[S->index_c(i,S->ny-1,k)] + S->value_e*u[S->index_e(i,S->ny-1,k)] + S->value_w*u[S->index_w(i,S->ny-1,k)] + S->value_s*u[S->index_s(i,S->ny-1,k)] + S->value_t*u[S->index_t(i,S->ny-1,k)] + S->value_b*u[S->index_b(i,S->ny-1,k)] ;
+		}
+	}
+	// nx-1, ny-1,  ...
+	#pragma omp parallel for schedule(static)
+	for(int k = 1; k < S->nz-1; k++){
+		v[S->index_c(S->nx-1,S->ny-1,k)] = S->value_c*u[S->index_c(S->nx-1,S->ny-1,k)] + S->value_w*u[S->index_w(S->nx-1,S->ny-1,k)] + S->value_s*u[S->index_s(S->nx-1,S->ny-1,k)] + S->value_t*u[S->index_t(S->nx-1,S->ny-1,k)] + S->value_b*u[S->index_b(S->nx-1,S->ny-1,k)];
+	}
+
+
+	//    0,    0, nz-1
+	v[S->index_c(0,0,S->nz-1)] = S->value_c*u[S->index_c(0,0,S->nz-1)] + S->value_e*u[S->index_e(0,0,S->nz-1)] + S->value_n*u[S->index_n(0,0,S->nz-1)] + S->value_b*u[S->index_b(0,0,S->nz-1)];
+	//  ...,    0, nz-1
+	#pragma omp parallel for schedule(static)
+	for(int i = 1; i < S->nx-1; i++){
+		v[S->index_c(i,0,S->nz-1)] = S->value_c*u[S->index_c(i,0,S->nz-1)] + S->value_e*u[S->index_e(i,0,S->nz-1)] + S->value_w*u[S->index_w(i,0,S->nz-1)] + S->value_n*u[S->index_n(i,0,S->nz-1)] + S->value_b*u[S->index_b(i,0,S->nz-1)];
+	}
+	// nx-1,    0, nz-1
+	v[S->index_c(S->nx-1,0,S->nz-1)] = S->value_c*u[S->index_c(S->nx-1,0,S->nz-1)] + S->value_w*u[S->index_w(S->nx-1,0,S->nz-1)] + S->value_n*u[S->index_n(S->nx-1,0,S->nz-1)] + S->value_b*u[S->index_b(S->nx-1,0,S->nz-1)];
+
+	//    0,  ..., nz-1
+	#pragma omp parallel for schedule(static)
+	for(int j = 1; j < S->ny-1; j++){
+		v[S->index_c(0,j,S->nz-1)] = S->value_c*u[S->index_c(0,j,S->nz-1)] + S->value_e*u[S->index_e(0,j,S->nz-1)] + S->value_n*u[S->index_n(0,j,S->nz-1)] + S->value_s*u[S->index_s(0,j,S->nz-1)] + S->value_b*u[S->index_b(0,j,S->nz-1)];
+	}  
+	//  ...,  ..., nz-1
+	#pragma omp parallel for schedule(static) collapse(2)
+	for(int j = 1; j < S->ny-1; j++){
+		for(int i = 1; i < S->nx-1; i++){
+			v[S->index_c(i,j,S->nz-1)] = S->value_c*u[S->index_c(i,j,S->nz-1)] + S->value_e*u[S->index_e(i,j,S->nz-1)] + S->value_w*u[S->index_w(i,j,S->nz-1)] + S->value_n*u[S->index_n(i,j,S->nz-1)] + S->value_s*u[S->index_s(i,j,S->nz-1)] + S->value_b*u[S->index_b(i,j,S->nz-1)];
+		}
+	}
+	// nx-1,  ..., nz-1
+	#pragma omp parallel for schedule(static)
+	for(int j = 1; j < S->ny-1; j++){
+		v[S->index_c(S->nx-1,j,S->nz-1)] = S->value_c*u[S->index_c(S->nx-1,j,S->nz-1)] + S->value_w*u[S->index_w(S->nx-1,j,S->nz-1)] + S->value_n*u[S->index_n(S->nx-1,j,S->nz-1)]+ S->value_s*u[S->index_s(S->nx-1,j,S->nz-1)] + S->value_b*u[S->index_b(S->nx-1,j,S->nz-1)];
+	}
+
+	//    0, ny-1, nz-1
+	v[S->index_c(0,S->ny-1,S->nz-1)] = S->value_c*u[S->index_c(0,S->ny-1,S->nz-1)] + S->value_e*u[S->index_e(0,S->ny-1,S->nz-1)] + S->value_s*u[S->index_s(0,S->ny-1,S->nz-1)] + S->value_b*u[S->index_b(0,S->ny-1,S->nz-1)];
+	//  ..., ny-1, nz-1
+	#pragma omp parallel for schedule(static)
+	for(int i = 1; i < S->nx-1; i++){
+	v[S->index_c(i,S->ny-1,S->nz-1)] = S->value_c*u[S->index_c(i,S->ny-1,S->nz-1)] + S->value_e*u[S->index_e(i,S->ny-1,S->nz-1)] + S->value_w*u[S->index_w(i,S->ny-1,S->nz-1)] + S->value_s*u[S->index_s(i,S->ny-1,S->nz-1)] + S->value_b*u[S->index_b(i,S->ny-1,S->nz-1)];
+	}
+	// nx-1, ny-1, nz-1
+	v[S->index_c(S->nx-1,S->ny-1,S->nz-1)] = S->value_c*u[S->index_c(S->nx-1,S->ny-1,S->nz-1)] + S->value_w*u[S->index_w(S->nx-1,S->ny-1,S->nz-1)] + S->value_s*u[S->index_s(S->nx-1,S->ny-1,S->nz-1)] + S->value_b*u[S->index_b(S->nx-1,S->ny-1,S->nz-1)];
+
 	return;
 }
+
